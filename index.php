821e6dbc6149ac9f3d5a7a0c865a70a917409879
David Mitchel <mitzip@cs.txstate.edu>
2012-06-25 11:00:43 -0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Pryvet: for secret messages.
+ *
+ * A safe way to send private information through any medium
+ * that can contain a hyperlink.
+ *
+ * @author DavidMitchel.com
+ * @license MIT License http://opensource.org/licenses/mit-license.php
+ */
+
+
+?>
+<!doctype html>
+<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8" lang="en"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js lt-ie9" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<title>Pryvet: for secret messages.</title>
+	<meta name="description" content="">
+	<meta name="author" content="David Mitchel">
+	<meta name="viewport" content="width=device-width">
+</head>
+<body>
+<!--[if lt IE 7]><p class=chromeframe>Your browser is <em>ancient!</em> <a href="http://browsehappy.com/">Upgrade to a different browser</a> or <a 
+href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to experience this site.</p><![endif]-->
+<header>
+
+
+
+</header>
+<div role="main">
+
+
+
+</div>
+<footer>
+
+
+
+</footer>
+</body>
+</html>
