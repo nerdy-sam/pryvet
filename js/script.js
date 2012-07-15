de8586f41cf06f9e41773e7b633b0ba73a57e2d5
David Mitchel <git@mitzip.com>
2012-07-14 20:44:13 -0500

    form test

--- js/script.js
+++ js/script.js
@@ -7,6 +7,7 @@ sjcl.random.startCollectors();
 $('#secret').submit(function() {
 	var secretKey = sjcl.codec.hex.fromBits(sjcl.hash.sha256.hash(sjcl.random.randomWords(4)));
 	var secretObj = jQuery.parseJSON(sjcl.encrypt(secretKey, document.secret.message.value));
-	alert(secretObj.ct);
+	var theSecret = [sjcl.codec.hex.fromBits(sjcl.hash.sha256.hash(secretObj.ct)),secretObj.ct];
+	alert(theSecret);
 	return false;
 });
